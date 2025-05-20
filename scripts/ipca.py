5f7c09b6c2256cf48c2dbc50cc8c7abff2b215c0
Wislley Costa <105369922+wislleycosta@users.noreply.github.com>
2025-05-20 00:18:46 -0300

    Create ipca.py

---
+++ scripts/ipca.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def baixar_ipca():
+    url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json"
+    df = pd.read_json(url)
+    df['data'] = pd.to_datetime(df['data'], dayfirst=True)
+    df['valor'] = df['valor'].str.replace(',', '.').astype(float)
+    return df
